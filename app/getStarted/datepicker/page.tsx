3a460b098b41d6c6ea749ab9fbd393f5ceaf4073
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-09-12 16:32:53 -0400

    updates to the datepicker showcase page

--- app/getStarted/datepicker/page.tsx
+++ app/getStarted/datepicker/page.tsx
@@ -99,7 +99,7 @@ const DatePickerShowcase: React.FC = () => {
 
   return (
     <div className="w-full h-screen overflow-scroll bg-background-dark p-8">
-      <h1 className="text-6xl font-bold bg-gradient-to-br from-primary-light via-secondary-light to-secondary-light bg-clip-text text-transparent mb-4">
+      <h1 className="lg:text-6xl text-4xl font-bold bg-gradient-to-br from-primary-light via-secondary-light to-secondary-light bg-clip-text text-transparent mb-4">
         DatePicker Showcase
       </h1>
 
