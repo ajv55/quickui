15b8146cc592cfdba3e0df728c9a2f7b9618561e
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-09-27 22:02:03 -0400

    tested feature section

--- __tests__/feature.test.tsx
+++ __tests__/feature.test.tsx
@@ -2,12 +2,24 @@ import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import FeaturesSection from '@/app/components/mainPage/feature';
 
+beforeAll(() => {
+    class IntersectionObserverMock {
+      observe = jest.fn();
+      unobserve = jest.fn();
+      disconnect = jest.fn();
+      takeRecords = jest.fn();
+      constructor() {}
+    }
+  
+    global.IntersectionObserver = IntersectionObserverMock as any;
+  });
+
 describe('feature section', () => {
 
     it('renders main heading', () => {
-        render(<FeaturesSection/>)
+        render(<FeaturesSection />)
 
-        const heading = screen.getByRole('heading', {name: 'Features'});
+        const heading = screen.getByRole('heading', {level: 2});
         expect(heading).toBeInTheDocument();
     })
 })
